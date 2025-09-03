b2c80566981081277ac70ed2320d1fc503a7c9db
Kunal Bhatia <vinod83bhatiya@gmail.com>
2025-09-03 21:13:04 +0530

    Update chat bot.py

--- python/chat bot.py
+++ python/chat bot.py
@@ -5,17 +5,18 @@ genai.configure(api_key=API_KEY)
 
 model = genai.GenerativeModel("gemini-2.0-flash")
 chat = model.start_chat()
-response = chat.send_message("Your Name Rubi-Chan from today")
+response = chat.send_message("Your Name is Kairu AI from today")
 while True:
     try:
         user = input("YOU : ").lower()
         if user == "exit":
-            print("BOT : Chat ended Goodbaye!")
+            print("Kairu : Chat ended Goodbaye!")
             break
         response = chat.send_message(user)
-        print("BOT :", response.text)
+        print("Kairu :", response.text)
     
     except ValueError:
         print("PLEASE ENTER VALID VALUE")
                 
+
 
